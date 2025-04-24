06bd98ae2c0aba5591fbc6e6f7fe95d9e4efa174
DenDevv <denisdev76@gmail.com>
2025-04-24 12:41:15 +0300

    Delete backend directory

--- backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Float
-from .database import Base
-
-class CarTracker(Base):
-    __tablename__ = "car_trackers"
-
-    tracking_number = Column(String(26), primary_key=True, index=True)
-    latitude = Column(Float)
-    longitude = Column(Float)
